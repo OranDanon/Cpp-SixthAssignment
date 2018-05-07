6275593cc1f67bed48f0c211f909114a3d5c1ac2
Oran <33609354+OranDanon@users.noreply.github.com>
2018-05-08 01:15:13 +0300

    Fixing Problems

--- Board.cpp
+++ Board.cpp
@@ -10,7 +10,7 @@ void Board::copyProxies(Board & b1, const Board& b2)
 {
 	if (&b1 == &b2)
 	{
-		throw exception("Cannot copy the same board to itself");
+		throw std::string("Cannot copy the same board to itself");
 	}
 	if (b1.prx_cap != b2.prx_cap)
 	{
